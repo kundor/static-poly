3c90dbbf2fe7a78eedf1e40e8ed00ba8d97b3a4c
Kolya Matteo <kundor@kundor.org>
2016-05-23 23:11:31 -0400

    Add complex number example

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ computes a polynomial product during compilation.
 Currently, only clang++ seems capable of compiling this; I tried with clang-703.0.31.
 `example.cpp` must be compiled with the option `-fconstexpr-steps=4194304`.
 GCC 5 and 6 have internal compiler errors; see [PR71226](https://gcc.gnu.org/bugzilla/show_bug.cgi?id=71226).
+
+The only requirements for the header itself are a C++14 standard libary
+and Boost (specifically Boost.Range, Boost.TypeTraits, Boost.Math, and Boost.MPL).
+These also suffice for the main example (`example.cpp`).
+
+For the complex example, this [static_math library](https://github.com/kundor/static_math)
+for compile-time mathematics support is required.
+The standard library complex class has constexpr constructors and `real()`, `imag()`
+accessors, but none of the operators are constexpr!
+This seems like something that could be changed.
---
+++ complex-example.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <boost/math/constants/constants.hpp> // pi
+#include <static_math/complex.h> // constexpr complex class
+#include "static_poly.hpp"
+
+using std::cout;
+
+/***  Nicer output for complex numbers ***/
+
+namespace smath { // so it can be found
+    std::ostream& operator << (std::ostream& os, const smath::complex<double>& cd) {
+        using ::detail::is_zero;
+        using ::detail::ifnotone;
+        if (is_zero(cd.real)) {// real part is zero or denorm (or ∞, NaN)
+            if (is_zero(cd.imag.value))
+                return os << '0';
+            return os << ifnotone(cd.imag.value) << 'i';
+        }
+        if (is_zero(cd.imag.value)) // imag part is zero or denorm (or ∞, NaN)
+            return os << cd.real;
+        os << '(' << cd.real;
+        if (cd.imag.value < 0.)
+            os << " - " << ifnotone(-cd.imag.value);
+        else
+            os << " + " << ifnotone(cd.imag.value);
+        return os << "i)";
+    }
+}
+
+/*** Greatest common divisor ***/
+constexpr int gcd(int m, int n) {
+   while (n != 0) {
+      m %= n;
+      if (m == 0) return n;
+      n %= m;
+    }
+    return m;
+}
+
+/*** The Nth cyclotomic polynomial, computed with complex numbers ***/
+template <int N>
+constexpr static_poly<smath::complex<double>, N+1> cyclotomic() {
+    static_assert(N > 0, "No nonpositive numbers please!");
+    using namespace boost::math::double_constants;
+
+    static_poly<smath::complex<double>, N+1> cyc{1};
+
+    for (int k = 1; k <= N; ++k) {
+        if (gcd(k, N) == 1) {
+            static_poly<smath::complex<double>, 2> x{-smath::polar(1., 2.*k*pi / N), 1};
+            cyc = detail::mul(cyc, x);
+        // alas, std::polar(1., 2.*k*pi / N) is not constexpr
+        // furthermore, std::cos/std::sin are not constexpr (why!?)
+        }
+    }
+    return cyc;
+}
+
+/*** Euler's totient function... ***/
+template <int N>
+constexpr int euler_totient() {
+    return cyclotomic<N>().degree();
+}
+
+template <int N>
+struct foo {
+    constexpr int val() const {
+        return N;
+    }
+};
+
+int main() {
+    constexpr auto phi1 = cyclotomic<1>();
+    constexpr auto phi2 = cyclotomic<2>();
+    constexpr auto phi3 = cyclotomic<3>();
+    constexpr auto phi4 = cyclotomic<4>();
+    constexpr auto phi5 = cyclotomic<5>();
+    constexpr auto phi6 = cyclotomic<6>();
+    constexpr auto phi7 = cyclotomic<7>();
+    constexpr auto phi8 = cyclotomic<8>();
+    constexpr auto phi9 = cyclotomic<9>();
+    constexpr auto phi35 = cyclotomic<35>();
+
+    cout << phi1 << '\n'
+         << phi2 << '\n'
+         << phi3 << '\n'
+         << phi4 << '\n'
+         << phi5 << '\n'
+         << phi6 << '\n'
+         << phi7 << '\n'
+         << phi8 << '\n'
+         << phi9 << "\n\n"
+         << phi1*phi2 << '\n'
+         << phi1*phi3 << '\n'
+         << phi1*phi2*phi4 << '\n'
+         << phi1*phi5 << '\n'
+         << phi1*phi2*phi3*phi6 << '\n'
+         << phi1*phi7 << '\n'
+         << phi1*phi2*phi4*phi8 << '\n'
+         << phi1*phi3*phi9 << "\n\n"
+         << phi35 << '\n';
+
+    /* Use euler's totient function as a template parameter! */
+    foo<euler_totient<76>()> myfoo;
+    cout << myfoo.val() << '\n';
+
+    return 0;
+}
+
+
